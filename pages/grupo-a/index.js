1579b8351ae6f8bdc085f7c7a7ff436626d5bacd
DanielCaz <cazarez.jorge.prog2019@gmail.com>
2021-09-16 17:51:07 -0500

    Implemented staticprops for a classes

--- pages/grupo-a/index.js
+++ pages/grupo-a/index.js
@@ -1,22 +1,45 @@
 import app from "../../firebase";
-import { getFirestore, getDocs, collection } from "firebase/firestore";
+import { getFirestore, getDoc, doc } from "firebase/firestore";
 
-const grupoA = ({ data }) => {
+const grupoA = ({
+  claseLengInt,
+  claseSim,
+  claseLengAut,
+  claseFund,
+  claseWeb,
+  claseArq,
+  claseTaller,
+}) => {
   return <div></div>;
 };
 
 export const getStaticProps = async () => {
   const db = getFirestore(app);
-  const data = { content: [] };
-  const querySnapshot = await getDocs(collection(db, "clases_a"));
 
-  querySnapshot.forEach((doc) => {
-    data.content.push({ id: doc.id, data: doc.data() });
-  });
+  let docSnap = await getDoc(doc(db, "clases_a/1ynnCIkZcJNZGckGLt61"));
+  const claseLengInt = { id: docSnap.id, data: docSnap.data() };
+  docSnap = await getDoc(doc(db, "clases_a/CZH2RkgHx4hVvZvKs6OS"));
+  const claseSim = { id: docSnap.id, data: docSnap.data() };
+  docSnap = await getDoc(doc(db, "clases_a/HgD1lbsha4mHQqWvhB1K"));
+  const claseLengAut = { id: docSnap.id, data: docSnap.data() };
+  docSnap = await getDoc(doc(db, "clases_a/hbIdsO5gAj5IHFtEb2pT"));
+  const claseFund = { id: docSnap.id, data: docSnap.data() };
+  docSnap = await getDoc(doc(db, "clases_a/t6TdDwuPwCjpKSJr4F7E"));
+  const claseWeb = { id: docSnap.id, data: docSnap.data() };
+  docSnap = await getDoc(doc(db, "clases_a/uGO3wyDBrQvWebzG3Yk8"));
+  const claseArq = { id: docSnap.id, data: docSnap.data() };
+  docSnap = await getDoc(doc(db, "clases_a/uKBgIWSvkKiMEeVQ6Iln"));
+  const claseTaller = { id: docSnap.id, data: docSnap.data() };
 
   return {
     props: {
-      data,
+      claseLengInt,
+      claseSim,
+      claseLengAut,
+      claseFund,
+      claseWeb,
+      claseArq,
+      claseTaller,
     },
   };
 };
