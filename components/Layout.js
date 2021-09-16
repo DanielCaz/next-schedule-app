c72e8dfaeba8b109c6c0f106ce5316fa58c6bba6
DanielCaz <cazarez.jorge.prog2019@gmail.com>
2021-09-16 09:10:55 -0500

    Added header & nav

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,42 @@
 import Meta from "./Meta";
+import Link from "next/link";
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <div>
       <Meta title="Horarios 5° ISC" />
       <header>
-        <h1>Horarios ISC</h1>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container-fluid">
+            <Link href="/">
+              <a className="navbar-brand">Horarios ISC</a>
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav">
+                <Link href="/grupo-a">
+                  <a className="nav-link">Grupo A</a>
+                </Link>
+                <Link href="/grupo-b">
+                  <a className="nav-link">Grupo B</a>
+                </Link>
+                <Link href="/mis-links">
+                  <a className="nav-link">Mis Links</a>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </nav>
       </header>
       {children}
       <script
@@ -14,7 +45,7 @@ const Layout = ({ children }) => {
         integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ"
         crossOrigin="anonymous"
       ></script>
-    </>
+    </div>
   );
 };
 
