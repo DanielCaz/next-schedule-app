bb50738a4975c0809fbb85a5768f5d1316861f31
DanielCaz <cazarez.jorge.prog2019@gmail.com>
2021-09-16 17:22:42 -0500

    Implemented data fetching for classes a

--- pages/grupo-a/index.js
+++ pages/grupo-a/index.js
@@ -1,5 +1,24 @@
-const grupoA = () => {
+import app from "../../firebase";
+import { getFirestore, getDocs, collection } from "firebase/firestore";
+
+const grupoA = ({ data }) => {
   return <div></div>;
+};
+
+export const getStaticProps = async () => {
+  const db = getFirestore(app);
+  const data = { content: [] };
+  const querySnapshot = await getDocs(collection(db, "clases_a"));
+
+  querySnapshot.forEach((doc) => {
+    data.content.push({ id: doc.id, data: doc.data() });
+  });
+
+  return {
+    props: {
+      data,
+    },
+  };
 };
 
 export default grupoA;
