da2556a6c4666270067dd83d5735b49853c4fd7b
DanielCaz <cazarez.jorge.prog2019@gmail.com>
2021-09-17 08:13:42 -0500

    Added schedule data for a class

--- pages/grupo-a/index.js
+++ pages/grupo-a/index.js
@@ -10,6 +10,173 @@ const grupoA = ({
   claseArq,
   claseTaller,
 }) => {
+  const claseLibre = {
+    id: "1",
+    name: "Libre",
+    meetingUrl: "",
+  };
+  const datosA = [
+    {
+      id: 1,
+      dia: "Lunes",
+      clases: [
+        {
+          id: 1,
+          clase: claseLengInt,
+          horas: 2,
+        },
+        {
+          id: 2,
+          clase: claseArq,
+          horas: 1,
+        },
+        {
+          id: 3,
+          clase: claseWeb,
+          horas: 1,
+        },
+        {
+          id: 4,
+          clase: claseSim,
+          horas: 2,
+        },
+        {
+          id: 5,
+          clase: claseLibre,
+          horas: 1,
+        },
+      ],
+    },
+    {
+      id: 2,
+      dia: "Martes",
+      clases: [
+        {
+          id: 1,
+          clase: claseLengAut,
+          horas: 2,
+        },
+        {
+          id: 2,
+          clase: claseArq,
+          horas: 1,
+        },
+        {
+          id: 3,
+          clase: claseWeb,
+          horas: 1,
+        },
+        {
+          id: 4,
+          clase: claseTaller,
+          horas: 2,
+        },
+        {
+          id: 5,
+          clase: claseLibre,
+          horas: 1,
+        },
+      ],
+    },
+    {
+      id: 3,
+      dia: "Miércoles",
+      clases: [
+        {
+          id: 1,
+          clase: claseLengInt,
+          horas: 2,
+        },
+        {
+          id: 2,
+          clase: claseFund,
+          horas: 1,
+        },
+        {
+          id: 3,
+          clase: claseArq,
+          horas: 1,
+        },
+        {
+          id: 4,
+          clase: claseTaller,
+          horas: 1,
+        },
+        {
+          id: 5,
+          clase: claseSim,
+          horas: 1,
+        },
+        {
+          id: 6,
+          clase: claseLibre,
+          horas: 1,
+        },
+      ],
+    },
+    {
+      id: 4,
+      dia: "Jueves",
+      clases: [
+        {
+          id: 1,
+          clase: claseLengAut,
+          horas: 2,
+        },
+        {
+          id: 2,
+          clase: claseFund,
+          horas: 1,
+        },
+        {
+          id: 3,
+          clase: claseArq,
+          horas: 2,
+        },
+        {
+          id: 4,
+          clase: claseSim,
+          horas: 1,
+        },
+        {
+          id: 5,
+          clase: claseWeb,
+          horas: 1,
+        },
+      ],
+    },
+    {
+      id: 5,
+      dia: "Viernes",
+      clases: [
+        {
+          id: 1,
+          clase: claseLengInt,
+          horas: 1,
+        },
+        {
+          id: 2,
+          clase: claseFund,
+          horas: 2,
+        },
+        {
+          id: 3,
+          clase: claseTaller,
+          horas: 1,
+        },
+        {
+          id: 4,
+          clase: claseSim,
+          horas: 1,
+        },
+        {
+          id: 5,
+          clase: claseWeb,
+          horas: 2,
+        },
+      ],
+    },
+  ];
   return <div></div>;
 };
 
