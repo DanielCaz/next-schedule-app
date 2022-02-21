f56d96bed603c49f25e7447111f22d21342490ac
DanielCaz <cazarez.jorge.prog2019@gmail.com>
2022-02-20 21:03:37 -0600

    Added better UI

--- components/TableCell.tsx
+++ components/TableCell.tsx
@@ -10,21 +10,40 @@ const TableCell = ({
   location: String;
 }) => {
   return (
-    <td colSpan={hours}>
-      {meetingUrl ? (
-        <a
-          className="text-decoration-none text-white"
-          href={meetingUrl}
-          target="_blank"
-          rel="noreferrer"
-        >
-          {name}
-          <p className="m-0 text-info">{location}</p>
-        </a>
-      ) : (
-        <span className="text-muted">{name}</span>
+    <>
+      <td>
+        {meetingUrl ? (
+          <a
+            className="text-decoration-none text-white"
+            href={meetingUrl}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {name}
+            <p className="m-0 text-info">{location}</p>
+          </a>
+        ) : (
+          <span className="text-muted">{name}</span>
+        )}
+      </td>
+      {hours > 1 && (
+        <td>
+          {meetingUrl ? (
+            <a
+              className="text-decoration-none text-white"
+              href={meetingUrl}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {name}
+              <p className="m-0 text-info">{location}</p>
+            </a>
+          ) : (
+            <span className="text-muted">{name}</span>
+          )}
+        </td>
       )}
-    </td>
+    </>
   );
 };
 
